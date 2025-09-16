a2705384850d4cbfcc40674bf536846cfba3f9c1
yusueon1111 <yusuyeon443@gmail.com>
2025-09-16 22:12:37 +0900

    # 디버깅용

--- src/main/java/org/example/project/ProjectApplication.java
+++ src/main/java/org/example/project/ProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.example.project")
 @EnableScheduling
 public class ProjectApplication {
 
